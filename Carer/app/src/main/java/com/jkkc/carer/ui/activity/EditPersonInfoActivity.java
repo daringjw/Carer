08428ac065d749121e73db738cf08988c5b2bf5d
guan <909569736@qq.com>
2018-05-26 15:56:27 +0800

    sweet alert dialog

--- Carer/app/src/main/java/com/jkkc/carer/ui/activity/EditPersonInfoActivity.java
+++ Carer/app/src/main/java/com/jkkc/carer/ui/activity/EditPersonInfoActivity.java
@@ -3,20 +3,73 @@ package com.jkkc.carer.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.jkkc.carer.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by Guan on 2018/5/26.
  */
 
-public class EditPersonInfoActivity extends AppCompatActivity{
+public class EditPersonInfoActivity extends AppCompatActivity {
+
+    private TextView mTvSex;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_edit_person_info);
+
+        ImageView ivBack = (ImageView) findViewById(R.id.ivBack);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+
+        mTvSex = (TextView) findViewById(R.id.tvSex);
+
+
+        LinearLayout llSex = (LinearLayout) findViewById(R.id.llSex);
+        llSex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new SweetAlertDialog(EditPersonInfoActivity.this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("修改性别")
+                        .setContentText("")
+                        .setConfirmText("男")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                                mTvSex.setText("男");
+
+                            }
+                        })
+                        .setCancelText("女")
+                        .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.cancel();
+
+                                mTvSex.setText("女");
+
+                            }
+                        })
+                        .show();
+
+            }
+        });
 
 
     }
